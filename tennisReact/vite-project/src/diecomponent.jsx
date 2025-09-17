bf7f98fc6685250838c246abbb465f74552df870
Fatna Gabani <fatnagabani7@gmail.com>
2025-09-17 11:23:36 -1100

    Add modified component

--- tennisReact/vite-project/src/diecomponent.jsx
+++ tennisReact/vite-project/src/diecomponent.jsx
@@ -1,6 +1,6 @@
 export default function Die (props){
     const styles ={
-     backgroundColor: props.isHeld ? "#59E391" :"white"
+     backgroundColor: props.isHeld ? "#09853bff" :"white"
     }
     return (
       
